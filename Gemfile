019510076b3b44afc4acdb47a19351fbe3c37968
i2bskn <i2bskn@gmail.com>
2013-07-11 11:28:22 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+ruby '2.0.0'
+
+gem 'rails', '4.0.0'
+gem 'sass-rails', '~> 4.0.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :production do
+  gem 'rails_12factor'
+  gem 'pg'
+end
+
+group :development, :test do
+  gem 'pry'
+  gem 'pry-doc'
+  gem 'pry-rails'
+  gem 'rspec-rails'
+  gem 'webrat'
+  gem 'factory_girl_rails'
+  gem 'sqlite3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaggedGist::Application.config.secret_key_base = '1403ff6447012fb7ef1cc53d21a36935171f132130489d4d457cea155197005dbdf1f0c42507f8d73c5c3bcd2d179426f87e0c1d7ef4a8b52f79e23368e6305a'
