c2796ee7e25fc8234e047dcec431959f99b13839
i2bskn <i2bskn@gmail.com>
2013-07-11 15:08:31 +0900

    install octokit

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 1.2'
 
 gem 'omniauth-github'
+gem 'octokit'
 
 group :doc do
   gem 'sdoc', require: false
--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,7 @@
 class RootController < ApplicationController
   def index
+    user = @current_user
+    # @client = Octokit::Client.new(login: user.nickname, oauth_token: user.access_token)
+    # @url = "https://gist.github.com/i2bskn/e52f88524864b71696dc.js"
   end
 end
