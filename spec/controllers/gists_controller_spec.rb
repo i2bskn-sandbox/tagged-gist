8d95a6463d01b6409d73fa559e11fb26f67f39ce
i2bskn <i2bskn@gmail.com>
2013-07-14 04:08:14 +0900

    add specs of GistsController#tagged

--- spec/controllers/gists_controller_spec.rb
+++ spec/controllers/gists_controller_spec.rb
@@ -34,7 +34,7 @@ describe GistsController do
     context "with bad request" do
       it "return http 404 if not exists gist" do
         Gist.should_receive(:find).and_raise(ActiveRecord::RecordNotFound)
-        get :show, {id: gist_object.id}, {user: gist_object.user.id}
+        get :show, {id: 1}, {user: gist_object.user.id}
         expect(response.status).to eq(404)
       end
 
@@ -42,6 +42,109 @@ describe GistsController do
         g = FactoryGirl.create(:gist)
         g.update_attributes!(public_gist: false)
         get :show, {id: g.id}, {user: gist_object.user.id}
+        expect(response.status).to eq(404)
+      end
+    end
+  end
+
+  describe "POST tagged" do
+    context "with sign in" do
+      it "requested tag should be created" do
+        g = gist_object
+        expect {
+          post :tagged, {id: g.id, name: "MyString"}, {user: g.user.id}
+        }.to change(Tag, :count).by(1)
+      end
+
+      it "return http redirect" do
+        post :tagged, {id: gist_object.id, name: "MyString"}, {user: gist_object.user.id}
+        expect(response).to be_redirect
+      end
+
+      it "should redirect to root_path" do
+        post :tagged, {id: gist_object.id, name: "MyString"}, {user: gist_object.user.id}
+        expect(response).to redirect_to(root_path)
+      end
+
+      it "new tag should not created if invalid name" do
+        g = gist_object
+        expect {
+          post :tagged, {id: g.id, name: "invalid_name_of_tag_over_20_characters"}, {user: g.user.id}
+        }.not_to change(Tag, :count)
+      end
+
+      it "return http redirect if invalid name" do
+        post :tagged, {id: gist_object.id, name: "invalid_name_of_tag_over_20_characters"}, {user: gist_object.user.id}
+        expect(response).to be_redirect
+      end
+
+      it "should redirect to root_path if invalid name" do
+        post :tagged, {id: gist_object.id, name: "invalid_name_of_tag_over_20_characters"}, {user: gist_object.user.id}
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "with not sign in" do
+      it "return http 404 if not sign in" do
+        post :tagged, {id: gist_object.id, name: "MyString"}
+        expect(response.status).to eq(404)
+      end
+    end
+
+    context "with bad request" do
+      it "return http 404 if not exists gist" do
+        Gist.should_receive(:find).and_raise(ActiveRecord::RecordNotFound)
+        post :tagged, {id: 1, name: "MyString"}, {user: gist_object.user.id}
+        expect(response.status).to eq(404)
+      end
+
+      it "return http 404 if othor user's gist" do
+        g = FactoryGirl.create(:gist)
+        post :tagged, {id: g.id, name: "MyString"}, {user: gist_object.user.id}
+        expect(response.status).to eq(404)
+      end
+    end
+  end
+
+  describe "DELETE untagged" do
+    let!(:tag) {FactoryGirl.create(:tag)}
+
+    context "with sign in" do
+      it "request tag should be deleted" do
+        expect {
+          delete :untagged, {id: tag.gist.id, name: tag.name}, {user: tag.user.id}
+        }.to change(Tag, :count).by(-1)
+      end
+
+      it "return http redirect" do
+        delete :untagged, {id: tag.gist.id, name: tag.name}, {user: tag.user.id}
+        expect(response).to be_redirect
+      end
+
+      it "should redirect to root_path" do
+        delete :untagged, {id: tag.gist.id, name: tag.name}, {user: tag.user.id}
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "with not sign in" do
+      it "return http 404 if not sign in" do
+        expect {
+          delete :untagged, {id: tag.gist.id, name: tag.name}
+        }.not_to change(Tag, :count)
+        expect(response.status).to eq(404)
+      end
+
+      it "Tag.where should not called" do
+        Tag.should_not_receive(:where)
+        delete :untagged, {id: tag.gist.id, name: tag.name}
+      end
+    end
+
+    context "with bad request" do
+      it "return http 404 if not exists gist" do
+        Gist.should_receive(:find).and_raise(ActiveRecord::RecordNotFound)
+        delete :untagged, {id: tag.gist.id, name: tag.name}, {user: tag.user.id}
         expect(response.status).to eq(404)
       end
     end
