d9cdae34790e12734a7165b6a2aeb14e0626c8e1
i2bskn <i2bskn@gmail.com>
2013-07-17 01:38:15 +0900

    add validate to Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,20 @@
 class Tag < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 20}
+  validate :duplicate_check
 
   belongs_to :user
   belongs_to :gist
+
+  class << self
+    def duplicate?(options={})
+      where("name = :name AND user_id = :user AND gist_id = :gist", options).exists?
+    end
+  end
+
+  private
+  def duplicate_check
+    if Tag.duplicate?(name: name, user: user_id, gist: gist_id)
+      errors.add(:name, "is a duplicated")
+    end
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -17,5 +17,22 @@ describe Tag do
       tag.name = "a" * 21
       expect(tag).not_to be_valid
     end
+
+    it "should not valid if duplicated tag" do
+      Tag.create!(name: tag.name, user_id: tag.user_id, gist_id: tag.gist_id)
+      expect(tag).not_to be_valid
+    end
+  end
+
+  describe ".duplicate?" do
+    it "should return nil if not exists" do
+      g = FactoryGirl.create(:gist)
+      expect(Tag.duplicate?(name: "not_exists", user: g.user.id, gist: g.id)).to be_nil
+    end
+
+    it "should not return nil if exists" do
+      t = FactoryGirl.create(:tag)
+      expect(Tag.duplicate?(name: t.name, user: t.user_id, gist: t.gist_id)).to eq(1)
+    end
   end
 end
